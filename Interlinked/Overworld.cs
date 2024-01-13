21f6fee1ea0d9f1764252e6511f48c058e8762d8
kofu145 <kofu145g@gmail.com>
2024-01-12 18:38:24 -0800

    added plyaer entity

--- Interlinked/Overworld.cs
+++ Interlinked/Overworld.cs
@@ -1,7 +1,24 @@
-﻿namespace Interlinked;
+﻿using GramEngine.ECS;
+using GramEngine.ECS.Components;
+
+namespace Interlinked;
 using GramEngine.Core;
 
 public class Overworld : GameState
 {
-    
+    private Entity playerEntity;
+    public override void Initialize()
+    {
+        base.Initialize();
+        playerEntity = new Entity();
+        playerEntity.AddComponent(new Sprite("Content/nyplayer.png"));
+        
+        
+        AddEntity(playerEntity);
+    }
+
+    public override void Update(GameTime gameTime)
+    {
+        base.Update(gameTime);
+    }
 }
